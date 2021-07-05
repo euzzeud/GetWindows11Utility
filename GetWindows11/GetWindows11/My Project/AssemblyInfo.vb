316d52ae75cbf52c1e18d31bfd5c813ad9e1dc19
Euzzeud <57187626+euzzeud@users.noreply.github.com>
2021-07-05 15:11:29 +0200

    Add VB Project solution and EXE file

---
+++ GetWindows11/GetWindows11/My Project/AssemblyInfo.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Resources
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Vérifiez les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("Get Windows 11")>
+<Assembly: AssemblyDescription("Get Windows 11 utility allows you to download the Windows 11 leak ISO file easily.")>
+<Assembly: AssemblyCompany("Euzzeud#6040")>
+<Assembly: AssemblyProduct("Get Windows 11")>
+<Assembly: AssemblyCopyright("© 2021 - Euzzeud#6040")>
+<Assembly: AssemblyTrademark("Get Windows 11")>
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("d7cb932d-b43e-4479-84f5-523ffb61c9bb")>
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: NeutralResourcesLanguage("en-US")>
